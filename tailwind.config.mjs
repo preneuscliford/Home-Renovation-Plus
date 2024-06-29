b1634cf41e403c87504ad5e286554d9b4750d646
Cliford Preneus <preneuscliford.com>
2024-06-29 16:27:06 -0300

    add navbar and hero section

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      primary: "#FF7849",
+      secondary: " #8265E6",
+      accentgreen: "#A2E8A0",
+      accentPr: "#FFF4F0",
+      accentSg: "#FEFFFC",
+      ctColor: "#F8F8FF",
+      accentblue: "#5D9CEC",
+      accentyellow: "#FFD700",
+      bgColor: "#F4F4F4",
+      textcolor: "#000000",
+      darktextcolor: "#4A4A4A",
+    },
+  },
+  plugins: [],
+};
